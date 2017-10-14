f9e96587e0bb831fcc972c9ff22b9a6afa77e2c8
Trzi891 <wy1079125249@icloud.com>
2017-10-15 01:35:39 +0300

    finished

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -66,10 +66,8 @@ fun digitNumber(n: Int): Int {
     var count = 0
     var number = Math.abs( n )
     if ( n == 0 ) return 1
-    for ( i in 1..Math.abs( n ) ){
-        if ( number >= 1 ){
-            count++
-        }
+    while ( number >= 1 ){
+        count++
         number /= 10
     }
     return count
@@ -122,8 +120,7 @@ fun lcm(m: Int, n: Int): Int {
 fun minDivisor(n: Int): Int {
     var d = 2
     val number = n
-    for( i in 2..n ){
-        if ( number % d != 0  )
+    while ( number % d != 0  ){
             d ++
     }
     return d
@@ -206,7 +203,34 @@ fun cos(x: Double, eps: Double): Double = TODO()
  * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
  * Не использовать строки при решении задачи.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var count = 0.0
+    var number = Math.abs( n )
+    var sum1 = 0
+    var sum2 = 0
+    var sum = sum1 + sum2
+    if ( n == 0 ) return 1
+    while ( number >= 1 ){
+        count++
+        number /= 10
+    }
+    for ( i in 1..(count / 2).toInt() ){
+            if ( count % 2 == 0.0 ){
+               sum1 +=  (( n % Math.pow( 10.0 , i - 0.0 ) ) % Math.pow( 10.0 , i - 1.0 ) * Math.pow( 10.0 ,count - i  )).toInt()
+                sum2 += ( ( ( n / Math.pow( 10.0 ,count - i - 2.0 ) ) % Math.pow( 10.0 ,i - 1.0 ) )* Math.pow( 10.0 ,i - 1.0 ) ).toInt()
+                return sum
+            }else{
+                i in 1..(( count - 1 ) / 2).toInt()
+                sum1 += (( n % Math.pow( 10.0 , i - 0.0 ) ) % Math.pow( 10.0 , i - 1.0 )* Math.pow( 10.0 ,count - i  )).toInt()
+                sum2 += ( ( ( n / Math.pow( 10.0 ,count - i - 2.0 ) ) % Math.pow( 10.0 ,i - 1.0 ) )* Math.pow( 10.0 ,i - 1.0 ) ).toInt()
+                sum = (sum1 + sum2 + n % Math.pow( 10.0 ,( count - 1.0 ) / 2.0 ) % 10 * Math.pow( 10.0 , ( count - 1.0 ) / 2.0 )).toInt()
+                return sum
+            }
+        }
+    return sum
+}
+
+
 
 /**
  * Средняя
